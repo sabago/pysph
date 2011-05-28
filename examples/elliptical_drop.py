5583f460c51df62c75f563e3b917432f0b21edb0
Kunal Puri <kunal.r.puri@gmail.com>
2011-05-28 19:20:37 +0530

    Adding OpenCL support for the elliptical drop test case.

--- examples/elliptical_drop.py
+++ examples/elliptical_drop.py
@@ -3,23 +3,29 @@
 import pysph.base.api as base
 import pysph.solver.api as solver
 
+dt = 1e-4
+tf = 0.00076
+
 app = solver.Application()
 app.process_command_line()
 
-particles = app.create_particles(False,
-    solver.fluid_solver.get_circular_patch, name='fluid', type=0)
+particles = app.create_particles(
+    False,
+    solver.fluid_solver.get_circular_patch, name='fluid', type=0,
+    locator_type=base.NeighborLocatorType.SPHNeighborLocator,
+    cl_locator_type=base.OpenCLNeighborLocatorType.LinkedListSPHNeighborLocator,
+    domain_manager_type=base.DomainManagerType.LinkedListManager)
 
 # use the solvers default cubic spline kernel
 s = solver.FluidSolver(dim=2, integrator_type=solver.RK2Integrator)
 
-s.set_final_time(0.00076)
-s.set_time_step(1e-5)
+s.set_time_step(dt)
+s.set_final_time(tf)
 
 app.set_solver(s)
 
-s.post_step_functions.append(solver.PrintNeighborInformation(count=20,
-                                                             path=app.path,
-                                                             rank=app.rank))
+# Print the output at every time step
+s.set_print_freq(1)
 
 app.run()
 
