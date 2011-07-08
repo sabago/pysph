ba8bd785bdfa44e108752441f0c3bedb4477a7d2
Pankaj Pandey <pankaj86@gmail.com>
2011-07-08 15:29:59 +0530

    added function in pysph_to_vtk to extract text timeseries data from .npz files

--- source/pysph/tools/pysph_to_vtk.py
+++ source/pysph/tools/pysph_to_vtk.py
@@ -7,7 +7,7 @@ from numpy import array, c_, ravel, load, zeros_like
 
 
 def write_vtk(data, filename, scalars=None, vectors={'V':('u','v','w')}, tensors={},
-              coords=('x','y','z'), **kwargs):
+              coords=('x','y','z'), dims=None, **kwargs):
     ''' write data in to vtk file
 
     Parameters
@@ -25,18 +25,23 @@ def write_vtk(data, filename, scalars=None, vectors={'V':('u','v','w')}, tensors
         mapping of tensor name to tensor component names to take from data
     coords : list
         the name of coordinate data arrays (default=('x','y','z'))
+    dims : 3 tuple
+        the size along the dimensions for (None means x.shape)
     **kwargs : extra arguments for the file writer
         example file_type=binary/ascii
 
-     '''
+    '''
     x = data[coords[0]]
     y = data.get(coords[1], zeros_like(x))
     z = data.get(coords[2], zeros_like(x))
 
-    dims = array([1,1,1])
-    dims[:x.ndim] = x.shape
-    sg = tvtk.StructuredGrid(points=c_[x.flat,y.flat,z.flat],dimensions=array(dims))
+    if dims is None:
+        dims = array([1,1,1])
+        dims[:x.ndim] = x.shape
+    else:
+        dims = array(dims)
 
+    sg = tvtk.StructuredGrid(points=c_[x.flat,y.flat,z.flat],dimensions=array(dims))
     pd = tvtk.PointData()
 
     if scalars is None:
@@ -48,17 +53,18 @@ def write_vtk(data, filename, scalars=None, vectors={'V':('u','v','w')}, tensors
 
     for vec,vec_vars in vectors.iteritems():
         u,v,w = [data[i] for i in vec_vars]
-        sg.point_data.vectors = c_[ravel(u),ravel(v),ravel(w)]
-        sg.point_data.vectors.name = vec
+        pd.vectors = c_[ravel(u),ravel(v),ravel(w)]
+        pd.vectors.name = vec
+        sg.point_data.add_array(pd.vectors)
 
     for ten,ten_vars in tensors.iteritems():
         vars = [data[i] for i in ten_vars]
         tensors = c_[[ravel(i) for i in vars]].T
-        sg.point_data.tensors = tensors
-        sg.point_data.tensors.name = ten
+        pd.tensors = tensors
+        pd.tensors.name = ten
+        sg.point_data.add_array(pd.tensors)
 
     write_data(sg, filename, **kwargs)
-
 
 
 def detect_vectors_tensors(keys):
@@ -172,7 +178,7 @@ def get_output_details(path):
     return solvers
 
 
-def pysph_to_vtk(path, merge_procs=False, skip_existing=True):
+def pysph_to_vtk(path, merge_procs=False, skip_existing=True, binary=True):
     ''' convert pysph output .npz files into vtk format
 
     Parameters
@@ -186,11 +192,17 @@ def pysph_to_vtk(path, merge_procs=False, skip_existing=True):
         skip files where corresponding vtk already exist
         this is useful if you've converted vtk files while a solver is running
         only want to convert the newly added files
-
+    binary : bool
+        whether to use binary format in vtk file
     The output vtk files are stored in a directory `solver_name` _vtk within
     the `path` directory
 
     '''
+    if binary:
+        data_mode = 'binary'
+    else:
+        data_mode = 'ascii'
+
     if merge_procs is True:
         # FIXME: implement
         raise NotImplementedError, 'merge_procs=True not implemented yet'
@@ -233,10 +245,57 @@ def pysph_to_vtk(path, merge_procs=False, skip_existing=True):
                     if 'w' not in arrs:
                         arrs['w'] = z
                     write_vtk(arrs, of+str(i),
-                              scalars=scalars, vectors=vectors, tensors=tensors)
+                              scalars=scalars, vectors=vectors, tensors=tensors,
+                              data_mode=data_mode)
                     times_file.write('%d\t%s\n'%(i,time))
 
         times_file.close()
+
+def extract_text(path, particle_idx, props=['x','y','u','v','p','rho','sigma00','sigma01','sigma11'], ent=None, solvers=None):
+    if solvers:
+        raise NotImplementedError
+    else:
+        solvers = get_output_details(path)
+
+    for solver, (procs, entities, times) in solvers.iteritems():
+        print 'converting solver:', solver
+        dir = os.path.join(path,solver+'_vtk')
+        if not os.path.exists(dir):
+            os.mkdir(dir)
+        procs = sorted(procs)
+        entities = sorted(entities)
+        times = sorted(times, key=float)
+        times_file = open(os.path.join(dir,'times'), 'w')
+        e = ent
+        if ent is None:
+            e = entities
+        for entity in entities:
+            if entity not in e:
+                continue
+            print '    entity:', entity
+            for proc in procs:
+                print '        proc:', proc
+                print '        timesteps:', len(times)
+                f = '%s_%s_%s_'%(solver,proc,entity)
+                of = os.path.join(dir,f)
+                files = [open(os.path.join(path,f+'%d.dat'%particle_id), 'w') for particle_id in particle_idx]
+                print files
+                for file in files:
+                    file.write('i\tt\t'+'\t'.join(props))
+                for i, time in enumerate(times):
+                    print '\r',i,
+                    d = load(os.path.join(path, f+time+'.npz'))
+                    s = '\n%d\t%s'%(i,time)
+                    for j,file in enumerate(files):
+                        file.write(s)
+                        for prop in props:
+                            file.write('\t')
+                            file.write(str(d[prop][particle_idx[j]]))
+                    
+                    d.close()
+                
+                for file in files:
+                    file.close()
 
 def test():
     l = ['x'+str(i) for i in range(3)]
