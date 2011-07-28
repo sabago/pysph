3ab85ac695cde807a8f88783cb95090bcd5e2d05
Kunal Puri <kunal.r.puri@gmail.com>
2011-07-28 10:25:33 +0530

    Fixing the crazy balls example.
    We need to add a Leonard Jones type potential force to the wall to prevent penetration.

--- examples/crazy_balls.py
+++ examples/crazy_balls.py
@@ -4,87 +4,73 @@ import numpy
 import pysph.base.api as base
 import pysph.solver.api as solver
 import pysph.sph.api as sph
+import pysph.tools.geometry_utils as geom
 
 from random import randint
+from numpy import random
 
-sin = numpy.sin
-cos = numpy.cos
-pi = numpy.pi
+np = 512
 
-r = 1.0
-theta = numpy.linspace(0, 2*pi, 101)
+def create_particles_3d(**kwargs):
 
-vel_scale = 0.1
+    x = random.random(np)
+    y = random.random(np)
+    z = random.random(np)
+
+    u = random.random(np) * 0
+    v = random.random(np) * 0
+    w = random.random(np) * 0
+
+    vol_per_particle = numpy.power(1.0/np, 1.0/3.0)
+    radius = 2 * vol_per_particle
+
+    print "Using smoothing length: ", radius
+
+    h = numpy.ones_like(x) * radius
+
+    fluid = base.get_particle_array(name="fluid", type=base.Fluid,
+                                    x=x, y=y, z=z,
+                                    u=u, v=v, w=w,
+                                    h=h)
+
+    x, y, z = geom.create_3D_tank(0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.025)
+    h = numpy.ones_like(x) * 0.2
+    wall = base.get_particle_array(name="wall", type=base.Solid,
+                                   x=x, y=y, z=z, h=h)
+
+    print "Number of wall particles = ", wall.get_number_of_particles()
+
+    return [fluid, wall]
 
 def create_particles_2d(**kwargs):
+
+    x = random.random(np)
+    y = random.random(np)
+
+    u = random.random(np) * 0
+    v = random.random(np) * 0
+
+    vol_per_particle = numpy.power(1.0/np, 1.0/2.0)
+    radius = 2 * vol_per_particle
+
+    print "Using smoothing length: ", radius
+
+    h = numpy.ones_like(x) * radius
+
+    fluid = base.get_particle_array(name="fluid", type=base.Fluid,
+                                    x=x, y=y,
+                                    u=u, v=v,
+                                    h=h)
+
+    x, y = geom.create_2D_tank(-0.15, -0.15, 1.15, 1.15, 0.025)
+    h = numpy.ones_like(x) * 0.1
+    wall = base.get_particle_array(name="wall", type=base.Solid,
+                                   x=x, y=y, h=h)
+
+    print "Number of wall particles = ", wall.get_number_of_particles()
+
+    return [fluid, wall]
     
-    # create the first ball
-    x = sin(theta) 
-    y = cos(theta)
-
-    vel = randint(1,5) * vel_scale
-
-    dx = x[1] - x[0]
-    h = numpy.ones_like(x)*2*dx
-    u = numpy.ones_like(x) * vel
-    v = numpy.ones_like(x) * vel
-
-    m = numpy.ones_like(x) * 100
-    
-    ball1 = base.get_particle_array(name="ball1", type=1, cl_precision="single",
-                                    x=x, y=y, m=m, h=h, u=u, v=v)
-
-    x = sin(theta) + 3
-    y = cos(theta)
-
-    vel = randint(1,5) * vel_scale
-
-    dx = x[1] - x[0]
-    h = numpy.ones_like(x)*2*dx
-    u = numpy.ones_like(x) * -vel
-    v = numpy.ones_like(x) * vel
-
-    m = numpy.ones_like(x) * 100
-    
-    ball2 = base.get_particle_array(name="ball2", type=1, cl_precision="single",
-                                    x=x, y=y, m=m, h=h, u=u, v=v)
-
-
-    x = sin(theta) + 3
-    y = cos(theta) + 3
-
-    vel = randint(1,5) * vel_scale
-
-    dx = x[1] - x[0]
-    h = numpy.ones_like(x)*2*dx
-    u = numpy.ones_like(x) * -vel
-    v = numpy.ones_like(x) * -vel
-
-    m = numpy.ones_like(x) * 100
-    
-    ball3 = base.get_particle_array(name="ball3", type=1, cl_precision="single",
-                                    x=x, y=y, m=m, h=h, u=u, v=v)
-
-
-    x = sin(theta)
-    y = cos(theta) + 3
-
-    vel = randint(1,5) * vel_scale
-
-    dx = x[1] - x[0]
-    h = numpy.ones_like(x)*2*dx
-    u = numpy.ones_like(x) * vel
-    v = numpy.ones_like(x) * -vel
-
-    m = numpy.ones_like(x) * 100
-    
-    ball4 = base.get_particle_array(name="ball4", type=1, cl_precision="single",
-                                    x=x, y=y, m=m, h=h, u=u, v=v)
-    
-
-
-    return [ball1, ball2, ball3, ball4]
-
 
 app = solver.Application()
 
@@ -92,8 +78,8 @@ s = solver.Solver(dim=2, integrator_type=solver.RK2Integrator)
 
 s.add_operation(solver.SPHIntegration(
 
-    sph.ArtificialPotentialForce.withargs(),
-    on_types=[1,], from_types=[1,],
+    sph.ArtificialPotentialForce.withargs(factorp=200.0, factorm=0.0),
+    on_types=[base.Fluid], from_types=[base.Fluid, base.Solid],
     updates=["u","v", "w"],
     id="potential")
 
@@ -102,8 +88,8 @@ s.add_operation(solver.SPHIntegration(
 
 s.add_operation(solver.SPHIntegration(
 
-    sph.ArtificialPositionStep.withargs(),
-    on_types=[1,],
+    sph.PositionStepping.withargs(),
+    on_types=[base.Fluid],
     updates=["x","y","z"],
     id="step")
 
@@ -114,7 +100,7 @@ app.setup(
     variable_h=False,
     create_particles=create_particles_2d)
 
-s.set_time_step(1e-2)
+s.set_time_step(1e-5)
 s.set_final_time(25)
 
 app.run()
--- source/pysph/sph/funcs/test_funcs.pyx
+++ source/pysph/sph/funcs/test_funcs.pyx
@@ -2,7 +2,7 @@
 
 import numpy
 from pysph.base.point cimport cPoint, cPoint_distance, cPoint_sub, \
-     cPoint_new, normalized
+     cPoint_new, cPoint_length, normalized
 
 from pysph.base.particle_array cimport ParticleArray, LocalReal
 from pysph.base.carray cimport DoubleArray, LongArray
@@ -21,7 +21,8 @@ cdef class ArtificialPotentialForce(SPHFunctionParticle):
     #cdef double gx, gy, gz
 
     def __init__(self, ParticleArray source, ParticleArray dest,
-                 bint setup_arrays=True, double fac=0.0, **kwargs):
+                 bint setup_arrays=True, double factorp=1.0,
+                 factorm = 1.0, **kwargs):
         
         SPHFunctionParticle.__init__(self, source, dest, setup_arrays,
                                      **kwargs)
@@ -29,7 +30,8 @@ cdef class ArtificialPotentialForce(SPHFunctionParticle):
         self.id = 'artificial_potential'
         self.tag = "velocity"
 
-        self.fac = fac
+        self.factorp = factorp
+        self.factorm = factorm
 
     def set_src_dst_reads(self):
         pass
@@ -42,88 +44,35 @@ cdef class ArtificialPotentialForce(SPHFunctionParticle):
 
     cdef void eval_nbr(self, size_t source_pid, size_t dest_pid,
                        KernelBase kernel, double *nr):
-        cdef double hab, q, w
+        cdef double hab, q, w, rab_norm
         cdef cPoint rab
 
-        if self.source.name is not self.dest.name:
+        self._src.x = self.s_x.data[source_pid]
+        self._src.y = self.s_y.data[source_pid]
+        self._src.z = self.s_z.data[source_pid]
+        
+        self._dst.x = self.d_x.data[dest_pid]
+        self._dst.y = self.d_y.data[dest_pid]
+        self._dst.z = self.d_z.data[dest_pid]
+        
+        hab = 0.5 * ( self.d_h.data[dest_pid] + \
+                      self.s_h.data[source_pid] )
+            
+        rab = cPoint_sub(self._dst, self._src)
+        rab_norm = cPoint_length(rab)
 
-            
-            self._src.x = self.s_x.data[source_pid]
-            self._src.y = self.s_y.data[source_pid]
-            self._src.z = self.s_z.data[source_pid]
-            
-            self._dst.x = self.d_x.data[dest_pid]
-            self._dst.y = self.d_y.data[dest_pid]
-            self._dst.z = self.d_z.data[dest_pid]
-
-            
-            hab = 0.5 * ( self.d_h.data[dest_pid] + \
-                          self.s_h.data[source_pid] )
-            
-	    #a-dest, b-src, rab = rb - ra, vector from a-->b
-            rab = cPoint_sub(self._src, self._dst)
+        if rab_norm > 1e-15:
             rab = normalized(rab)
 	    
+        q = cPoint_distance(self._src, self._dst)/hab
+        w = kernel.function(self._dst, self._src, hab)
 
-            q = cPoint_distance(self._src, self._dst)/hab
-            
-            w = -100.0 * kernel.function(self._dst, self._src, hab)
-            #large distance - attraction
-            if q > 1:   
-                w = w * -1.0 * 0.01
+        if q > 0.5:
+            w *= self.factorm
+        else:
+            w *= self.factorp
 
-            nr[0] += rab.x * w
-            nr[1] += rab.y * w
-            nr[2] += rab.z * w
-
-#############################################################################
-# `ArtificialPositionStep` class.
-#############################################################################
-cdef class ArtificialPositionStep(SPHFunction):
-
-    def __init__(self, ParticleArray source, ParticleArray dest, 
-                 bint setup_arrays=True, **kwargs):
-
-        SPHFunction.__init__(self, source, dest, setup_arrays)
-
-        self.id = 'netpot'
-        self.tag = "velocity"
-
-
-    def set_src_dst_reads(self):
-        pass
-
-    cpdef eval(self, KernelBase kernel, DoubleArray output1,
-               DoubleArray output2, DoubleArray output3):
-
-        cdef size_t i
-        
-        # get the tag array pointer
-        cdef LongArray tag_arr = self.dest.get_carray('tag')
-
-        self.setup_iter_data()
-        cdef size_t np = self.dest.get_number_of_particles()
-        cdef size_t a
-
-        cdef DoubleArray u = self.d_u
-        cdef DoubleArray v = self.d_v
-        cdef DoubleArray w = self.d_w
-
-        cdef cPoint _sum = cPoint_new(0,0,0)
-
-        for a in range(np):
-            if tag_arr.data[a] == LocalReal:
-                _sum.x = _sum.x + u.data[a]
-                _sum.y = _sum.y + v.data[a]
-                _sum.z = _sum.z + w.data[a]
-
-        _sum.x = _sum.x/np
-        _sum.y = _sum.y/np
-        _sum.z = _sum.z/np
-
-        for a in range(np):
-            if tag_arr.data[a] == LocalReal:
-                output1.data[a] = _sum.x
-                output2.data[a] = _sum.y
-                output3.data[a] = _sum.z
+        nr[0] += rab.x * w
+        nr[1] += rab.y * w
+        nr[2] += rab.z * w
 
